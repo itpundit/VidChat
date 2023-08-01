8e0bc654ba9eddcc79db2374e0ada559a32d4a9c
Gihan Gamage <gihangamage.15@cse.mrt.ac.lk>
2023-08-01 18:56:02 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 import streamlit as st
+import whisper
+import torch
+import os
+from pytube import YouTube
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.document_loaders import DataFrameLoader
+from langchain.vectorstores import Chroma
+from langchain.chains import RetrievalQAWithSourcesChain
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.llms import OpenAI
+import pandas as pd
+
+
+
+
+
 from PIL import Image
 import os
 # import langchain
@@ -28,6 +44,28 @@ st.set_page_config(layout="centered", page_title="Website QnA")
 #     st.header('Geek Avenue')
 # st.write("---") # horizontal separator line.
 
+def extract_and_save_audio(video_URL, destination, final_filename):
+  video = YouTube(video_URL)#get video
+  audio = video.streams.filter(only_audio=True).first()#seperate audio
+  output = audio.download(output_path = destination)#download and save for transcription
+  _, ext = os.path.splitext(output)
+  new_file = final_filename + '.mp3'
+  os.rename(output, new_file)
+
+def chunk_clips(transcription, clip_size):
+  texts = []
+  sources = []
+  for i in range(0,len(transcription),clip_size):
+    clip_df = transcription.iloc[i:i+clip_size,:]
+    text = " ".join(clip_df['text'].to_list())
+    source = str(round(clip_df.iloc[0]['start']/60,2))+ " - "+str(round(clip_df.iloc[-1]['end']/60,2)) + " min"
+    print(text)
+    print(source)
+    texts.append(text)
+    sources.append(source)
+
+  return [texts,sources]
+
 
 st.header("Website QnA")
 state = st.session_state
@@ -45,37 +83,68 @@ if st.button("Build Model"):
     try:
 
       my_bar = st.progress(0, text="Crawling in progress. Please wait.")
-      if os.path.exists("simp.jl"):
-        os.remove("simp.jl")
+      # Set the device
+      device = "cuda" if torch.cuda.is_available() else "cpu"
       
-      adv.crawl(site, 'simp.jl', follow_links=False)
+      # Load the model
+      whisper_model = whisper.load_model("base", device=device)
       st.write(str(site) + " starting to crawl..")
-      crawl_df = pd.read_json('simp.jl', lines=True)
-      st.write(len(crawl_df))
-      crawl_df = crawl_df[['body_text']]
+     
+      # Video to audio
+      video_URL = 'https://www.youtube.com/watch?v=oG7uCemfJgU'
+      destination = "."
+      final_filename = "Geek_avenue"
+      extract_and_save_audio(video_URL, destination, final_filename)
+
+      # run the whisper model
+      audio_file = "Geek_avenue.mp3"
       my_bar.progress(50, text="Building Vector DB.")
-      st.write(crawl_df)
+      result = whisper_model.transcribe(audio_file)
+      transcription = pd.DataFrame(result['segments'])
+
+      chunks = chunk_clips(transcription, 50)
+      documents = chunks[0]
+      sources = chunks[1]
+
+
+
+      embeddings = OpenAIEmbeddings(openai_api_key = st.secrets["openai_api_key"])
+      #vstore with metadata. Here we will store page numbers.
+      vStore = Chroma.from_texts(documents, embeddings, metadatas=[{"source": s} for s in sources])
+      #deciding model
+      model_name = "gpt-3.5-turbo"
+      
+      retriever = vStore.as_retriever()
+      retriever.search_kwargs = {'k':2}
+
+      model = RetrievalQAWithSourcesChain.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever=retriever)
+     
+      # crawl_df = pd.read_json('simp.jl', lines=True)
+      # st.write(len(crawl_df))
+      # crawl_df = crawl_df[['body_text']]
+      # my_bar.progress(50, text="Building Vector DB.")
+      # st.write(crawl_df)
 
       #load df to langchain
-      loader = DataFrameLoader(crawl_df, page_content_column="body_text")
-      docs = loader.load()
+      # loader = DataFrameLoader(crawl_df, page_content_column="body_text")
+      # docs = loader.load()
 
-      #chunking
-      text_splitter = RecursiveCharacterTextSplitter(
-      chunk_size=1000, chunk_overlap=0, separators=[" ", ",", "\n"]
-      )
-      doc_texts = text_splitter.split_documents(docs)
+      # #chunking
+      # text_splitter = RecursiveCharacterTextSplitter(
+      # chunk_size=1000, chunk_overlap=0, separators=[" ", ",", "\n"]
+      # )
+      # doc_texts = text_splitter.split_documents(docs)
 
 
       #extract embeddings and build QnA Model
-      openAI_embeddings = OpenAIEmbeddings(openai_api_key = st.secrets["openai_api_key"])
-      vStore = Chroma.from_documents(doc_texts, openAI_embeddings)
+      # openAI_embeddings = OpenAIEmbeddings(openai_api_key = st.secrets["openai_api_key"])
+      # vStore = Chroma.from_documents(doc_texts, openAI_embeddings)
 
       # Initialize VectorDBQA Chain from LangChain
       #deciding model
-      model_name = "gpt-3.5-turbo"
-      llm = OpenAI(model_name=model_name, openai_api_key = st.secrets["openai_api_key"])
-      model = VectorDBQA.from_chain_type(llm=llm, chain_type="stuff", vectorstore=vStore)
+      # model_name = "gpt-3.5-turbo"
+      # llm = OpenAI(model_name=model_name, openai_api_key = st.secrets["openai_api_key"])
+      # model = VectorDBQA.from_chain_type(llm=llm, chain_type="stuff", vectorstore=vStore)
       my_bar.progress(100, text="Model is ready.")
       st.session_state['crawling'] = True
       st.session_state['model'] = model
