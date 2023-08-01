5ce09d034c882c6e5fe7a7492a0f57e1458ff30c
Gihan Gamage <gihangamage.15@cse.mrt.ac.lk>
2023-08-01 21:16:20 +1000

    Update main.py

--- main.py
+++ main.py
@@ -41,12 +41,12 @@ st.set_page_config(layout="centered", page_title="Website QnA")
 # image = Image.open('GeekAvenue_logo.png')
  
 
-# col1, mid, col2 = st.columns([1,2,20])
-# with col1:
-#     st.image(image, width=80)
-# with col2:
-#     st.header('Geek Avenue')
-# st.write("---") # horizontal separator line.
+col1, mid, col2 = st.columns([1,2,20])
+with col1:
+    st.image(image, width=80)
+with col2:
+    st.header('Geek Avenue')
+st.write("---") # horizontal separator line.
 
 def extract_and_save_audio(video_URL, destination, final_filename):
   video = YouTube(video_URL)#get video
